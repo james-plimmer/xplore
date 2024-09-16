15026da42fc3289f1921c03b11040d0f7364075d
James Plimmer <sc21jpwp@leeds.ac.uk>
2024-09-16 10:13:08 +0100

    Initial Commit

---
+++ xplore/xploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  xploreApp.swift
+//  xplore
+//
+//  Created by James Plimmer on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct xploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
